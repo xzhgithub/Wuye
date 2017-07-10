d363d3c2e48bc58041525187f024025421fda840
xzhgithub <924670581@qq.com>
2017-07-10 10:03:45 +0800

    5

--- onethink-master/wwwroot/Application/Common/Conf/config.php
+++ onethink-master/wwwroot/Application/Common/Conf/config.php
@@ -17,7 +17,7 @@ return array(
 
     /* 模块相关配置 */
     'AUTOLOAD_NAMESPACE' => array('Addons' => ONETHINK_ADDON_PATH), //扩展模块列表
-    'DEFAULT_MODULE'     => 'Home',
+    'DEFAULT_MODULE'     => 'Wechat',
     'MODULE_DENY_LIST'   => array('Common','User','Admin','Install'),
     //'MODULE_ALLOW_LIST'  => array('Home','Admin'),
 
