2e27fc369de2f5f83a70cd129d621bc9631132ae
xzhgithub <924670581@qq.com>
2017-07-10 09:55:32 +0800

    4

---
+++ onethink-master/wwwroot/Application/Wechat/Controller/WechatController.class.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 12195
+ * Date: 2017/7/10
+ * Time: 9:15
+ */
+
+namespace Wechat\Controller;
+
+
+class WechatController
+{
+    public function index(){
+        echo 111;
+    }
+
+}
